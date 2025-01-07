ff91a7e7971798bc907c54431178333a8483ba6c
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-07 22:00:00 +0530

    Add GrapesJS template

---
+++ src/templates/grapesjs.template.ts
@@ -0,0 +1,18 @@
+export const GRAPES_JS_TEMPLATE = ` <div class="solias-designer">
+      <div id="tool-bar" class="solias-designer__panel-top">
+        <div id="tool-actions" class="solias-designer__actions"></div>
+        <div id="panel-devices" class="solias-designer__devices"></div>
+        <div id="panel-switcher" class="solias-designer__switcher"></div>
+      </div>
+      <div id="solias-workspace" class="solias-designer__workspace">
+        <div id="gjs" class="solias-designer__canvas">
+          <!-- Canvas Area -->
+        </div>
+        <div id="right-panel" class="solias-designer__panel-right">
+          <div id="blocks-palette" class="solias-designer__blocks"></div>
+          <div id="style-palette" class="solias-designer__styles"></div>
+          <div id="layers-palette" class="solias-designer__layers"></div>
+          <div id="traits-palette" class="solias-designer__traits"></div>
+        </div>
+      </div>
+    </div>`;
