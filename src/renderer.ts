ce7eac578a2b2c1ec39507fa40b8ffd4206220ed
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-09 22:00:00 +0530

    Add GrapesJS styles

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,10 @@
 /* Solias Styles */
 import './scss/index.scss';
 
+/* GrapesJS Default Styles */
+import 'grapesjs/dist/css/grapes.min.css';
+/* Solias Styles */
+import '@berkelium/primordial-theme/dist/styles.min.css';
+
+console.log('👋 Welcome to Solias');
+
