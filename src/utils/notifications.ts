e61212feb06a9a99821d03090627867b7dea732b
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-12 22:00:00 +0530

    generate docs for Notification utility

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,6 +1,11 @@
 import { Notification, NotificationConstructorOptions } from 'electron';
 
 export class SoliasNotifications {
+    /**
+     * Shows a notification with the given options
+     * @param args The options to use when creating the notification
+     *              (see https://electronjs.org/docs/api/notification#new-notificationoptions)
+     */
     show(args: NotificationConstructorOptions): void {
         args.icon = 'src/icon.png';
         const notify = new Notification(args);
