8cf0680004dd3d61e7491be401cafd110345e118
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-15 22:00:00 +0530

    Move GrapesJS UI Styles

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,7 +5,7 @@ import './scss/index.scss';
 import 'grapesjs/dist/css/grapes.min.css';
 /* Solias Styles */
 // import '@berkelium/primordial-theme/dist/styles.min.css';
-import './css/grapejs-ui.css';
+import '../assets/css/grapejs-ui.css';
 
 console.log('👋 Welcome to Solias');
 
